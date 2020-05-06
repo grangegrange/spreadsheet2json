4b5c717f0db0d5a56ef0aead7c221f51f4a30140
igor grange <igorgrange@gmail.com>
2020-05-06 05:09:10 +0200

    adding fonts & styles constants and styling resizer

--- src/App.js
+++ src/App.js
@@ -33,47 +33,51 @@ class App extends Component {
                         onJsonResult={jsonData => this.setState({ jsonData })}
                     />
 
-                    <div className={styles.editors}>
-                        {
-                            ((tabledData.length > 0) || Object.keys(jsonData).length > 0)
-                                ? <ResizablePanels
-                                    bkcolor="#ffffff"
-                                    displayDirection="column"
-                                    width="100%"
-                                    height="100vh"
-                                    panelsSize={[50, 50]}
-                                    sizeUnitMeasure="%"
-                                    resizerColor="#ff9900"
-                                    resizerSize="10px"
-                                >
-                                    {
-                                        (tabledData.length > 0)
-                                            ? <Table
-                                                data={tabledData}
-                                                onSelect={selectedTableValue => this.setState({selectedTableValue})}
-                                                style={{
-                                                    background: "#ffffff",
-                                                    height: "100%",
-                                                    width: "100%"
-                                                }}
-                                            />
-                                            : <div>Загрузите таблицу</div>
-                                    }
-                                    {
-                                        (Object.keys(jsonData).length > 0)
-                                            ?   <JsonWrapper
-                                                data={jsonData}
-                                                style={{
-                                                    background: "#ffffff",
-                                                    height: "100%"
-                                                }}
-                                            />
-                                            :   <div>Загрузите JSON</div>
-                                    }
-                                </ResizablePanels>
-                                : null
-                        }
-                    </div>
+
+                        {/*{*/}
+                            {/*((tabledData.length > 0) || Object.keys(jsonData).length > 0)*/}
+                                {/*?*/}
+                                <div className={styles.editors}>
+                                    <ResizablePanels
+                                        bkcolor="#ffffff"
+                                        displayDirection="column"
+                                        width="100%"
+                                        height="100vh"
+                                        panelsSize={[50, 50]}
+                                        sizeUnitMeasure="%"
+                                        resizerColor="#6c7ae0"
+                                        resizerSize="2px"
+                                    >
+                                        {
+                                            (tabledData.length > 0)
+                                                ? <Table
+                                                    data={tabledData}
+                                                    onSelect={selectedTableValue => this.setState({selectedTableValue})}
+                                                    style={{
+                                                        background: "#ffffff",
+                                                        height: "100%",
+                                                        width: "100%"
+                                                    }}
+                                                />
+                                                : <div className={styles.editorIsEmpty}>Загрузите таблицу</div>
+                                        }
+                                        {
+                                            (Object.keys(jsonData).length > 0)
+                                                ?   <JsonWrapper
+                                                    data={jsonData}
+                                                    style={{
+                                                        background: "#ffffff",
+                                                        height: "100%"
+                                                    }}
+                                                />
+                                                :   <div className={styles.editorIsEmpty}>Загрузите JSON</div>
+                                        }
+                                    </ResizablePanels>
+                                  </div>
+
+                                {/*: null*/}
+                        {/*}*/}
+
 
                 </div>
             </div>
